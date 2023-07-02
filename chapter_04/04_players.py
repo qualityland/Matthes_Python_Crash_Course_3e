9e81519ac5e3e6d6b0dfdc9c6cbd158a5c5f5c41
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2023-07-02 10:54:13 +0200

    chapter 4 slices

---
+++ chapter_04/04_players.py
@@ -0,0 +1,14 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players)
+
+# first three
+print(players[0:3])
+
+# 3rd to end
+print(players[2:])
+
+# 1st to 3rd
+print(players[:4])
+
+# last three
+print(players[-3:])
