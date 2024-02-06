4b399dc99e2d4ab5238a77a99208af8b15e306f9
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2024-02-06 09:10:29 +0100

    exercise updated

--- chapter_05/exercise_05-01.py
+++ chapter_05/exercise_05-01.py
@@ -2,11 +2,13 @@
 # Write a series of conditional tests. Print a statement describing each test
 # and your prediction for the results of each test. Your code should look
 # something like this:
-    car = 'subaru'
-    print("Is car == 'subaru'? I predict True.")
-    print(car == 'subaru')
-    print("\nIs car == 'audi'? I predict False.")
-    print(car == 'audi')
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
 # - Look closely at your results, and make sure you understand why each line
 #   evaluates to True or False.
 # - Create at least 10 tests. Have at least 5 tests evaluate to True and another
